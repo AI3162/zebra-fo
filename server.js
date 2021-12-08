0e74f68e884cb16855bad652474eadd1644708bb
AI3162 <77072768+AI3162@users.noreply.github.com>
2021-12-08 20:45:37 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static('./dist/FrontOffice'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', {root: 'dist/FrontOffice/'}),
+  res.sendFile('./dist/FrontOffice/index.html'),
 );
 
 // Start the app by listening on the default Heroku port
